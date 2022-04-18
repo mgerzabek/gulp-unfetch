dd06181886da46db92f7dcf1a02ab7691a480958
Michael Gerzabek <michael.gerzabek@gmail.com>
2022-04-18 16:04:03 +0200

    Bennen der function

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fetch = require('node-fetch')
 
 const PLUGIN_NAME = 'gulp-unfetch'
 
-module.exports = (options = {}) =>
+module.exports = function unfetch (options = {})
 {
 
   return new Transform({
